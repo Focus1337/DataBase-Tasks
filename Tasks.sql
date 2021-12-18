57a67ca7e1c04f8894b81a569f724db4ddce38a4
Focus <maniaclord@bk.ru>
2021-12-18 17:18:40 +0300

    task 2 rik fix

--- Tasks.sql
+++ Tasks.sql
@@ -23,20 +23,14 @@ WHERE consumption > 20
 AND EXISTS (SELECT mat_code FROM Materials WHERE price > 100)
 AND cost.oper_num = 2 -- определяется операцией
 
-SELECT 5 FORALL 1 EXISTS 4
+-- По условиям задачи: SELECT 5 FORALL 1 EXISTS 4
 
 -- RA
 [det_code, oper_num, worker_code, worker_qualif, tariff_code, pf_time, piece_time]
 ([oper_num = value](ManufacturingCosts))*([consumption > 20](MaterialsConsumption)) *([price > 100](Materials))
 
---5. Вывести покупателей, которые купили в этом году все товары меньше 100
-
---       НАЙТИ{ (c.name) | c in Cust}
---                FORALL (p in Prod) p.price < 100  ->
---                  ( EXISTS(o in Ord) o.id_cust=c.id & o.ip_prod=p.id )
-
 -- RIK
 НАЙТИ{(c.det_code, c.oper_num, c.worker_code, c.worker_qualif, c.tariff_code, c.pf_time, c.piece_time) | c in ManufacturingCosts} 
-		FORALL (m in Materials) m.price > 100 &
+		FORALL (m in Materials) m.price > 100 ->
 		( EXISTS(mc in MaterialsConsumption) mc.det_code = c.det_code & mc.mat_code = m.mat_code & mc.consumption > 20 ) &
 		c.oper_num = value
