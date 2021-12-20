4edb2988854fbf5729cc505da5de45f3197a82a9
Focus <maniaclord@bk.ru>
2021-12-20 21:29:36 +0300

    fixed bugs in Task14. Added first version of Task21

--- Third semester/Database Design/Task14.sql
+++ Third semester/Database Design/Task14.sql
@@ -24,7 +24,7 @@ CREATE TABLE Products
 CREATE TABLE DishProducts
 (
 	dish_id INT,
-	product_id INT
+	product_id INT,
 
 	-- блюдо может состоять из нескольких продуктов. Продукт может входить в состав нескольких блюд;
 	--!! Поэтому ставлю первичный и внешний ключ на dish_id и product_id,
@@ -32,11 +32,11 @@ CREATE TABLE DishProducts
 	
 	CONSTRAINT pk_dp PRIMARY KEY(dish_id, product_id),
 
-	CONSTRAINT pk_dp_dish FOREIGN KEY(dish_id)
+	CONSTRAINT fk_dp_dish FOREIGN KEY(dish_id)
 			REFERENCES Dishes(id)
 			on delete cascade on update cascade,
 
-	CONSTRAINT pk_dp_product FOREIGN KEY(product_id)
+	CONSTRAINT fk_dp_product FOREIGN KEY(product_id)
 			REFERENCES Products(id)
 			on delete cascade on update cascade
 )
@@ -46,12 +46,12 @@ CREATE TABLE Recipes
 (
 	dish_id INT,
 	[time] int,
-	technology nvarchar
+	technology nvarchar,
 
 	-- блюдо имеет один рецепт. Рецепт соответствует одному блюду. 
 	--!! Поэтому ставлю первичный и внешний ключ на dish_id, который указывает на id у Dishes (Связь один к одному)
 	CONSTRAINT pk_recipe PRIMARY KEY(dish_id),
-	CONSTRAINT pk_recipe_dish FOREIGN KEY(dish_id)
+	CONSTRAINT fk_recipe_dish FOREIGN KEY(dish_id)
 			REFERENCES Dishes(id)
 			on delete cascade on update cascade,
 )
@@ -62,12 +62,12 @@ CREATE TABLE Cooking
 	cooking_id INT CONSTRAINT pk_cooking PRIMARY KEY,
 	dish_id INT,
 	portion_count int,
-	[date] date
+	[date] date,
 
 	-- у блюда может быть несколько приготовлений (оно может приготавливаться в разные дни в некотором количестве порций). 
 	-- Приготовление соотносится с одним блюдом; 
 	--!! Поэтому ставлю внешний ключ на dish_id, который указывает на id у Dishes  (Связь один ко многим)
-	CONSTRAINT pk_cooking_dish FOREIGN KEY(dish_id)
+	CONSTRAINT fk_cooking_dish FOREIGN KEY(dish_id)
 			REFERENCES Dishes(id)
 			on delete cascade on update cascade
 )
